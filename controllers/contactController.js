07eaddae7f744b1f34331bbdae88901a9717d4bd
Axel MASSON <SIAM3684@SITES.fr>
2020-03-11 10:24:12 +0100

    adding contacts ways api

---
+++ controllers/contactController.js
@@ -0,0 +1,19 @@
+Contact = require('../models/contactModel');
+
+exports.index = function(req, res) {
+    Contact.get(function(err, contact)
+    {
+        if(err){
+            res.json({
+                status: "error",
+                message: err
+            });
+        }
+        res.json({
+            status: "success",
+            message: "contacts informations retrieved successfully",
+            data: contact
+        });
+    });
+
+};
