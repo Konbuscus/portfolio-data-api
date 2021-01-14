216c13ad3928b96d92b96d61db8a924f72da9253
Konbuscus <axel.masson.pro@gmail.com>
2021-01-14 20:23:03 +0100

    model correction

--- models/cryptoModel.js
+++ models/cryptoModel.js
@@ -7,7 +7,7 @@ var cryptoSchema = mongoose.Schema({
         required: true
     },
     crypto_last_price_eur : {
-        type: Number,
+        type: SchemaTypes.Double
         required: true
     },
     crypto_last_price_usd : {
