cab0f193cca6990e4e7b6de49f5606cd38ccfdc0
Konbuscus <axel.masson.pro@gmail.com>
2021-01-14 20:04:13 +0100

    byte optimisation

--- models/cryptoModel.js
+++ models/cryptoModel.js
@@ -3,15 +3,15 @@ var mongoose = require('mongoose');
 var cryptoSchema = mongoose.Schema({
 
     crypto_last_date:{
-        type: String,
+        type: Number,
         required: true
     },
     crypto_last_price_eur : {
-        type: String,
+        type: Number,
         required: true
     },
     crypto_last_price_usd : {
-        type: String,
+        type: Number,
         required: true
     },
     crypto_name:{
@@ -26,3 +26,4 @@ var Crypto = module.exports = mongoose.model('cryptohistory', cryptoSchema);
 module.exports.get = function (callback, limit){
     Crypto.find(callback).limit(limit);
 }
+
